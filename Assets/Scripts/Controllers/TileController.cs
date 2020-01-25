bdd623482c791439af53621f55f107789ffa9354
Kristian <kristian.zh.apostolov@gmail.com>
2020-01-25 17:55:05 +0200

    Dynamic creation of tile map

---
+++ Assets/Scripts/Controllers/TileController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour {
+
+	public Vector3 startPos;
+	public Vector3 endPos;
+	public GameObject tile;
+
+	private List<List<Tile>> tileMap = new List<List<Tile>>();
+	void Start () {
+		float tileScale = tile.GetComponent<Transform>().localScale.x;
+		
+		float xDiff = endPos.x - startPos.x;
+		float zDiff = endPos.z - startPos.z;
+		int xDir = xDiff > 0 ? 1 : -1;
+		int zDir = zDiff > 0 ? 1 : -1;
+		int tileNumX = (int)( Mathf.Abs(xDiff)/tileScale ) + 1;
+		int tileNumZ = (int)( Mathf.Abs(zDiff)/tileScale ) + 1;
+
+		for (int z = 0; z < tileNumZ; z++) {
+			List<Tile> row = new List<Tile>();
+			for (int x = 0; x < tileNumX; x++) {
+				GameObject tileGO = Instantiate(tile, startPos + new Vector3(x * tileScale * xDir, 0, z * tileScale * zDir), Quaternion.identity);
+				row.Add(tileGO.GetComponent<Tile>());
+			}
+			tileMap.Add(row);
+		}
+
+		print(tileMap[0][0].IsWall());
+	}
+	
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,22 +6,23 @@ public class Tile : MonoBehaviour {
 
 	private Material material;
 	private bool isWall = false;
+	private float alpha = 0.3f;
 
 	public void Start() {
 		material = GetComponent<Renderer>().material;
 	}
 
 	public void OnTriggerStay(Collider other) {
-		if (CollisionController.IsIgnored(other.tag)) {
+		if (CollisionController.IsIgnored(other.tag) || !material) {
 			return;
 		}
 		isWall = true;
-		material.SetColor("_Color", new Color(1, 0, 0, .4f));
+		material.SetColor("_Color", new Color(1, 0, 0, alpha));
 	}
 
 	public void OnTriggerExit() {
 		isWall = false;
-		material.SetColor("_Color", new Color(1, 1, 1, .4f));
+		material.SetColor("_Color", new Color(1, 1, 1, alpha));
 	}
 
 	public bool IsWall() {
