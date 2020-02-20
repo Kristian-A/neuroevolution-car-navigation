c8c6cf3a52918a8322f9c435dc46a1c756910230
Kristian <kristian.zh.apostolov@gmail.com>
2020-02-20 14:20:05 +0200

    Started work on GA

---
+++ Assets/Scripts/AI/GeneticAlgorithm.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+
+class GeneticAlgorithm {
+
+    public class Entry {
+
+        public List<Double> dna;
+        private double fitness;
+        public Entry(List<Double> dna, double fitness) {
+            this.dna = dna;
+            this.fitness = fitness;
+        }
+
+        public double GetProbability(double sumFitness) {
+            return fitness/sumFitness;
+        }
+
+        public double GetFitness() {
+            return fitness;
+        }
+
+        public List<Double> GetDna() {
+            return dna;
+        }
+    }
+
+    private static Random generator = new Random();
+    public static List<double>[] Crossover(List<double> dna1, List<double> dna2) {
+        List<double> child1 = new List<double>();
+        List<double> child2 = new List<double>();
+
+        int intersection = generator.Next(dna1.Count);
+        List<double> parent1 = dna1;
+        List<double> parent2 = dna2;
+
+        for (int i = 0; i < dna1.Count; i++) {
+            if (i > intersection) {
+                parent1 = dna2;
+                parent2 = dna1;
+            }
+
+            child1.Add(parent1[i]);
+            child2.Add(parent2[i]);
+        }
+
+        return new[] { Mutate(child1), Mutate(child2) };
+    }   
+
+    private static List<Double> Mutate(List<Double> dna) {
+        const double mutationRate = 0.001;
+
+        if (generator.NextDouble() < mutationRate) {
+            int toSwap1 = generator.Next(dna.Count);
+            int toSwap2 = generator.Next(dna.Count);
+
+            double gene = dna[toSwap1];
+            dna[toSwap1] = dna[toSwap2];
+            dna[toSwap2] = gene;
+        }
+
+        return dna;
+    }
+
+    public static Entry Pick(List<Entry> entries) {
+
+        double sumFitness = 0;
+        foreach (Entry entry in entries) {
+            sumFitness += entry.GetFitness();
+        }
+
+        double previousProb = 0; 
+ 
+        foreach (Entry entry in entries) {
+            double currentProb = entry.GetProbability(sumFitness); 
+
+            if (generator.NextDouble() < currentProb + previousProb) {
+                entry.dna.Add(currentProb);
+                return entry;
+            }
+            previousProb += currentProb;
+        }
+
+        return null;
+    }
+}
--- Assets/Scripts/Controllers/AIController.cs
+++ Assets/Scripts/Controllers/AIController.cs
@@ -7,17 +7,47 @@ public class AIController : MonoBehaviour {
 	private static bool createdBrains = false;
 
     public void Start() {
-		NeuralNetwork m = new NeuralNetwork(1, 2, 2);
+		// NeuralNetwork m = new NeuralNetwork(1, 2, 2);
+		// List<double> a = new List<double>();
+		// int size = m.DNA().Count;
+		// for (int i = 0; i < size; i++) {
+		// 	a.Add(i);
+		// }
+
+		// m.SetWeights(a);
+
+		
+	
 		List<double> a = new List<double>();
-		int size = m.DNA().Count;
-		for (int i = 0; i < size; i++) {
-			a.Add(i);
-		}
+		List<double> b = new List<double>();
+		List<double> c = new List<double>();
+		
 
-		m.SetWeights(a);
+		a.Add(0);
+		a.Add(1);
+		a.Add(2);
 
-		foreach (double num in m.DNA()) {
-			
+		b.Add(-1);
+		b.Add(-2);
+		b.Add(-3);
+
+		c.Add(4);
+		c.Add(4);
+		c.Add(4);
+
+		GeneticAlgorithm.Entry e1 = new GeneticAlgorithm.Entry(a, 0.2);
+		GeneticAlgorithm.Entry e2 = new GeneticAlgorithm.Entry(b, 0.3);
+		GeneticAlgorithm.Entry e3 = new GeneticAlgorithm.Entry(c, 0.5);
+		
+		List<GeneticAlgorithm.Entry> l = new List<GeneticAlgorithm.Entry>();
+
+		l.Add(e1);
+		l.Add(e2);
+		l.Add(e3);
+
+		GeneticAlgorithm.Entry res = GeneticAlgorithm.Pick(l);
+
+		foreach (double num in res.GetDna()) {
 			print(num);
 		}
 	}
